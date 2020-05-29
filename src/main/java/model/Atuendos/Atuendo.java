c1611c70ff65f2e141e24326bf3079fbc57a9c5f
Nicolás Suarez <cristianicolasuarez97105@gmail.com>
2020-05-29 16:13:05 -0300

    Creo la clase Atuendo, que sera un conjunto de prendas.
    Conjunto coherente, de momento una prenda de cada tipo, en un futuro puede cambiar.

---
+++ src/main/java/model/Atuendos/Atuendo.java
@@ -0,0 +1,13 @@
+package model.Atuendos;
+
+import java.util.List;
+
+public class Atuendo
+{
+    public List<Prenda> prendas;
+
+    public Atuendo(List<Prenda> prendas)
+    {
+        this.prendas=prendas;
+    }
+}
